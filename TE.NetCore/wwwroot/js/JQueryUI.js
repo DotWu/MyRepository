29e897d62b6ec9fa5fab8d9e54aff9aa0c07d5d2
DotWu <30611222+DotWu@users.noreply.github.com>
2019-01-07 23:17:34 +0800

    Dot
    20190107

---
+++ TE.NetCore/wwwroot/js/JQueryUI.js
@@ -0,0 +1,35 @@
+﻿$(document).ready(function () {
+    $("#idBtn").button();
+    $("#rect1").draggable();   ///可以拖动
+    $("#rect2").droppable();   ///可放置drop
+    $("#rect2").on("drop", function (event, ui) {
+        $("#rect1").text("放置drop事件");
+    });
+    $("#rect3").resizable();  ///可以改变款高大小
+    $("#sortable").sortable();
+
+    $("#btn").button();
+    $("#select").selectable();
+    $("#btn").on("click", function () {
+        if ($("#right").hasClass("ui-selected")) {
+            alert("恭喜你答对了！");
+        } else {
+            alert("对不起，你答错了！");
+        }
+    });
+    $("#Accordion").accordion();
+
+    var autotags = ["html", "css", "javascript", "JQuery", "C#", "C++", "C", "java", "PHP", "asp.net", "Python"];
+    $("#tags").autocomplete({
+        source: autotags
+    });
+
+    $("#datepicker").datepicker();
+});
+
+$(function () {
+    $("#btnDiglog").button().on("click", function () {
+        $("#dialog").dialog();
+    });
+});
+
